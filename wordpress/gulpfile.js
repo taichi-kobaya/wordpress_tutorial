535c07e92f80fce4c7c6daa378fcef7e57a4e9e0
taichi-kobaya <taichi.kobayashi@eeeeg.tokyo>
2021-04-19 11:46:45 +0900

    SCSS動作確認
    gulpfile.js修正

--- wordpress/gulpfile.js
+++ wordpress/gulpfile.js
@@ -1,16 +1,16 @@
 const { watch, series, task, src, dest } = require('gulp');
-const scss                               = require('gulp-scss');
+const scss                               = require('gulp-sass'); // gulp-scssでは対応できなかった為
 const autoprefixer                       = require('gulp-autoprefixer');
 const plumber                            = require('gulp-plumber');
- 
+
 const convertToCss = () =>
   src('./src/scss/style.scss')
       .pipe(plumber())
       .pipe(scss())
       .pipe(autoprefixer())
-      .pipe(dest('./wp-content/themes/xeory_extention-child'));
- 
+      .pipe(dest('./wp-content/themes/xeory_extension-child'));
+
 const watchScss = () =>
   watch('./src/scss/**/*.scss', convertToCss);
- 
+
 exports.default = watchScss;
